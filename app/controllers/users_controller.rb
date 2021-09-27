39241b365ef334849b9ceadae26eebe0f26cb1c0
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-09-27 11:43:00 +0000

    [add]users_index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,10 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @hums = @user.hums
-  end
-
-  def edit
-  end
-
-  def update
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
   get 'homes/about' => 'homes#about'
   resources :hums
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show]
 end
