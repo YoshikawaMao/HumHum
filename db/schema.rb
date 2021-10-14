bce497c3f09566e30857e484904c61ebadf38e0e
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-10-14 11:15:40 +0000

    [update]HumComment_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_081715) do
+ActiveRecord::Schema.define(version: 2021_10_14_111348) do
 
   create_table "genres", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2021_10_12_081715) do
     t.text "content"
     t.string "name"
     t.integer "user_id"
+  end
+
+  create_table "hum_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "hums", force: :cascade do |t|
