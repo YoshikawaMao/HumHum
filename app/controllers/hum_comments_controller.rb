fa2654ff63345175bff3ab27216b0de86c94b575
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-10-16 16:42:35 +0000

    [add]hum_comment

--- app/controllers/hum_comments_controller.rb
+++ app/controllers/hum_comments_controller.rb
@@ -1,5 +1,10 @@
 class HumCommentsController < ApplicationController
   def create
+    @hum = Hum.find(params[:hum_id])
+    comment = current_user.hum_comments.new(hum_comment_params)
+    comment.hum_id = hum_id
+    comment.save
+    redirect_to hum_path(hum.id)
   end
 
   def destroy
